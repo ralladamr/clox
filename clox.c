a90e10441f9174c7f9caa5bbb08372f69bd254fd
Adam Rall <ralladamr@gmail.com>
2024-01-26 12:52:43 -0500

    Don't reuse constant

--- clox.c
+++ clox.c
@@ -7,24 +7,27 @@ int main(int argc, char* argv[])
     init_VM();
     Chunk chunk;
     init_chunk(&chunk);
-    int constant = add_constant(&chunk, 1.2);
+    
+    int a = add_constant(&chunk, 1.2);
     write_chunk(&chunk, op_constant, 123);
-    write_chunk(&chunk, constant, 123);
+    write_chunk(&chunk, a, 123);
 
-    constant = add_constant(&chunk, 3.4);
+    int b = add_constant(&chunk, 3.4);
     write_chunk(&chunk, op_constant, 123);
-    write_chunk(&chunk, constant, 123);
+    write_chunk(&chunk, b, 123);
 
     write_chunk(&chunk, op_add, 123);
 
-    constant = add_constant(&chunk, 5.6);
+    int c = add_constant(&chunk, 5.6);
     write_chunk(&chunk, op_constant, 123);
-    write_chunk(&chunk, constant, 123);
+    write_chunk(&chunk, c, 123);
 
     write_chunk(&chunk, op_divide, 123);
     write_chunk(&chunk, op_negate, 123);
     write_chunk(&chunk, op_return, 123);
+    
     interpret(&chunk);
+    
     free_VM();
     free_chunk(&chunk);
     return 0;
